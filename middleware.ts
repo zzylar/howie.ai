47d5195fba5e9561f85947829225922965161f93
Howie Huntington <rhuntington2716@gmail.com>
2023-07-30 13:27:34 -0400

    block /chat route with middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,40 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: Request) {
-  const origin = request.headers.get('origin');
+const isChatRoute = (pathname: string) => {
+  return pathname.startsWith("/chat");
+}
+
+export function middleware(request: NextRequest) {
+  const origin = request.headers.get("origin");
   console.log(origin);
 
-  const allowedOrigins = ['*'];
+  const { pathname } = new URL(request.nextUrl);
+
+  const allowedOrigins = ["*"];
   const isOriginAllowed = origin !== null && allowedOrigins.includes(origin);
 
   const response = NextResponse.next();
 
-  if (isOriginAllowed) {
-    response.headers.set('Access-Control-Allow-Origin', origin!); // Using "!" to assert that origin is not null
+  if (isChatRoute(pathname)) {
+    return NextResponse.redirect(new URL('/api/auth/unauthorized', request.url));
   }
 
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  response.headers.set('Access-Control-Max-Age', '86400');
+  if (isOriginAllowed) {
+    response.headers.set("Access-Control-Allow-Origin", origin!); // Using "!" to assert that origin is not null
+  }
 
-  console.log('Middleware!');
+
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  response.headers.set("Access-Control-Max-Age", "86400");
+
+  console.log("Middleware!");
   console.log(request.method);
   console.log(request.url);
 
@@ -25,5 +42,5 @@ export function middleware(request: Request) {
 }
 
 export const config = {
-  matcher: '/api/:path*',
+  matcher: ["/api/:path*", "/chat/:path*"]
 };
