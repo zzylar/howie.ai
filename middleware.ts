752b97735921bb51eece0a773a028255c98acf2b
Howie Huntington <rhuntington2716@gmail.com>
2023-07-30 14:09:56 -0400

    secure /chat with redirect in next.config

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const isChatRoute = (pathname: string) => {
-  console.log('isChatRoute', pathname);
-  return pathname.startsWith("/chat");
-}
-
 export function middleware(request: NextRequest) {
   const origin = request.headers.get("origin");
-  console.log(origin);
-  console.log('Request Object', request);
-
-  const { pathname } = new URL(request.nextUrl);
 
   const allowedOrigins = ["*"];
   const isOriginAllowed = origin !== null && allowedOrigins.includes(origin);
 
   const response = NextResponse.next();
 
-  console.log('Auth Header Check', request.headers.get('Authorization'));
-  console.log('Pathname', pathname);
-  if (isChatRoute(pathname)) {
-    return NextResponse.redirect(new URL('/api/auth/unauthorized', request.url));
-  }
 
   if (isOriginAllowed) {
     response.headers.set("Access-Control-Allow-Origin", origin!); // Using "!" to assert that origin is not null
@@ -38,13 +24,9 @@ export function middleware(request: NextRequest) {
   );
   response.headers.set("Access-Control-Max-Age", "86400");
 
-  console.log("Middleware!");
-  console.log(request.method);
-  console.log(request.url);
-
   return response;
 }
 
 export const config = {
-  matcher: ["/api/:path*", "/chat/:path*"]
+  matcher: "/api/:path*"
 };
