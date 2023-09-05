e07148ce1e04b8c11b7f590c89b39bda22364452
Howie Huntington <rhuntington2716@gmail.com>
2023-09-04 20:10:08 -0400

    remove unused Nav from old project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Nav from "../components/Nav";
 import AuthProvider from "./providers/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,7 +24,6 @@ export default function RootLayout({
         <title>Howie AI</title>
       </head>
         <body className={`${inter.className} flex flex-col h-screen bg-gray-100`}>
-          {/* <Nav /> */}
           <div className="max-w-[95%] ml-6 mr-6 mt-8 sm:ml-8 sm:mr-8 sm:mt-10 md:ml-16 md:mr-16 md:mt-16 lg:ml-[20%] lg:mr-[20%] lg:mt-[10%]">
             {children}
           </div>
